675cfcadeb5d4934400d360a0e398c2ecee30caa
KENNETH PHANG <bunnyppl@hotmail.com>
2022-07-15 09:00:27 +0800

    new

---
+++ src/main/java/vttp2022b2/webapp/day14revision/Day14revisionApplication.java
@@ -0,0 +1,13 @@
+package vttp2022b2.webapp.day14revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day14revisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day14revisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022b2/webapp/day14revision/Day14revisionApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022b2.webapp.day14revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day14revisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
